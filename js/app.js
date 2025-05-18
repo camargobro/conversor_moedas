f5363e63361575ee933daae0de70958a72296831
Gustavo Camargo <gu_camargo06@hotmail.com>
2025-05-18 00:55:46 -0300

    Implementando sistema de moeda destino e moeda origem ao conversor.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,35 @@
 function converterMoeda(){
 
-const moeda = document.getElementById('moeda').value;
+const moedaDestino = document.getElementById('moeda_destino').value;
+const moedaOrigem = document.getElementById('moeda_origem').value;
 const quantidadeConverter = document.getElementById('converter').value;
 
-if(moeda === "dolar"){
-    const valorDolar = 5.25;
-    const dolarConvertido = quantidadeConverter * valorDolar;
-    const nomeDolar = quantidadeConverter > 1 ? "dólares" : "dólar";
-   document.getElementById('texto_conversao').innerHTML = `
-<h1 class="texto_conversao">A conversão de ${quantidadeConverter} ${nomeDolar} para Real é de ${dolarConvertido}</h1>`;
-}else if(moeda === "euro"){
-    const cotacaoEuro = 6.37;
-    const euroConvertido = quantidadeConverter * cotacaoEuro;
-    const nomeEuro = quantidadeConverter > 1 ? "euros" : "euro";
-    document.getElementById('texto_conversao').innerHTML = `
-<h1 class="texto_conversao">A conversão de ${quantidadeConverter} ${nomeEuro} para Real é de ${euroConvertido}</h1>`;
-} else{
-    return alert('Selecione uma moeda válida');
+if (moedaDestino === "" || moedaOrigem === "") {
+    alert("Escolha as moedas de origem e destino!");
+    return;  
+} else if( quantidadeConverter === "") {
+    alert("Escolha a quantidade a ser convertida!");
+    return;
+} else if(moedaDestino === moedaOrigem){
+ alert("As moedas de origem e destino não podem ser iguais!");
+  return;
 }
+
+const taxasMoedas = {
+  BRL: { USD: 1 / 5.25, EUR: 1 / 6.37 },
+  USD: { BRL: 5.25, EUR: 0.85 },
+  EUR: { BRL: 6.37, USD: 1.18 }
+};
+const taxaConversao = taxasMoedas[moedaDestino][moedaOrigem];
+const valorConversao = taxaConversao * quantidadeConverter;
+
+   document.getElementById('texto_conversao').innerHTML = `
+<h1 class="texto_conversao">A conversão de ${quantidadeConverter} ${moedaDestino} para ${moedaOrigem} é de ${valorConversao}</h1>`;
 
 }
 function limpar(){
-    document.getElementById('converter').value = "";
-    document.getElementById('moeda').value = "";
     document.getElementById('texto_conversao').innerHTML = "Nenhuma moeda convertida até o momento!";
+    document.getElementById('moeda_destino').value = "";
+    document.getElementById('moeda_origem').value = "";
+    document.getElementById('converter').value = "";
 }
